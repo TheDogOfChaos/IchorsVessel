b57152b9398c74839bbff34904201974f0785697
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-05-18 20:40:32 +0100

    jdjsadjkasdjksad
    Signed-off-by: Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>

---
+++ localisation/default.lua
@@ -0,0 +1,14 @@
+return {
+    descriptions = {
+        Joker = {
+            ichors_vessel_fear = {
+                name = "{C:red,E:2}Fear{}",
+                text = {
+                    "At the end of a hand, {C:red,E:2}Force Trigger{} the effects of the Jokers to the immediate left and right.",
+                    "Force Triggering ignores all activation conditions, and apply [r]+1 Dread[/] to each affected card.\r\n[l](Only affects Jokers depicting a living person)[/]\r\n\r\n[o]Jokers[/] with [r]6\u002B Dread[/] become [3]Debuffed[/] until their [r]Dread[/] is reduced to 0.\r\nUnaffected [o]Jokers[/] lose [r]1 Dread[/] after each hand."
+                
+                }
+            }
+        }
+    }
+}
--- main.lua
+++ main.lua
@@ -1,1 +1,26 @@
 print("Loading Ichor's Vessel...")
+--[[
+SMODS.Atlas {
+	-- Key for code to find it with
+	key = "CardAtlas",
+	-- The name of the file, for the code to pull the atlas from
+	path = "assets/CardAtlas.png",
+	-- Width of each sprite in 1x size
+	px = 69,
+	-- Height of each sprite in 1x size
+	py = 93
+}
+]]
+
+SMODS.Joker {
+    key = 'fear',
+    rarity = 3,
+    -- atlas = "CardAtlas",
+    -- pos = { x = 0, y = 0 },
+    cost = 0,
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = false,
+}
+
+print("Loaded Ichor's Vessel!")
