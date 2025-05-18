addf470925378dafabba2176f7f1eb312788945b
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-05-18 21:08:07 +0100

    formatting
    Signed-off-by: Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -4,9 +4,11 @@ return {
             j_ichors_vessel_fear = {
                 name = "{C:red,E:2}Fear{}",
                 text = {
-                    "At the end of a hand, {C:red,E:2}Force Trigger{} the effects of the Jokers to the immediate left and right.",
-                    "Force Triggering ignores all activation conditions, and apply [r]+1 Dread[/] to each affected card.\r\n[l](Only affects Jokers depicting a living person)[/]\r\n\r\n[o]Jokers[/] with [r]6\u002B Dread[/] become [3]Debuffed[/] until their [r]Dread[/] is reduced to 0.\r\nUnaffected [o]Jokers[/] lose [r]1 Dread[/] after each hand."
-                
+                    "At the end of a hand, {C:attention,E:2}Force Trigger{} the Joker to the left and right",
+                    "{C:inactive}(Only affects Jokers depicting a living person){}",
+                    "Affected cards gain +1 {C:red,E:2}Dread{}",
+                    "Jokers with 6+ {C:red,E:2}Dread{} become {X:mult,C:white}Debuffed{} until their {C:red,E:2}Dread{} is reduced to 0",
+                    "Jokers lose 1 {C:red,E:2}Dread{} per hand without being {C:attention,E:2}Force Triggered{} by {C:red,E:2}Fear{}"
                 }
             }
         }
